6438d0d4e632bae78d040b5f6eabb0af4fba54c3
Lucas Sá <130018028+lucasdpsa01@users.noreply.github.com>
2026-05-07 22:37:49 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,8 @@
 import { Sequelize } from 'sequelize'
-import dotenv from 'dotenv';
 
 dotenv.config(); 
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(postgres://postgres:123456@localhost:5432/user, {
     dialect: 'postgres',
     logging: false,
 });
